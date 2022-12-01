852bb9f5237ce0da6ceba41948e3e0d9486d5e12
ismail0vv <asylismailov05@gmail.com>
2022-12-01 10:35:55 +0600

    homework 4

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -3,17 +3,12 @@ from rest_framework.routers import SimpleRouter
 
 from .views import *
 
-movie_router = SimpleRouter()
-movie_router.register(prefix='movies', viewset=MovieViewSet)
-print(movie_router.urls)
-director_router = SimpleRouter()
-director_router.register(prefix='directors', viewset=DirectorViewSet)
-
-review_router = SimpleRouter()
-review_router.register(prefix='reviews', viewset=ReviewViewSet)
-
 urlpatterns = [
-    path('', include(movie_router.urls)),
-    path('', include(director_router.urls)),
-    path('', include(review_router.urls))
+    path('movies/', movie_view),
+    path('movies/<int:id>/', movie_detail_view),
+    path('directors/', director_view),
+    path('directors/<int:id>/', director_detail_view),
+    path('reviews/', review_view),
+    path('reviews/<int:id>/', review_detail_view),
+    path('movies/reviews/', movies_reviews_view)
 ]
