5ce9928f61ffd2ace7c0f20012e66cf728ec4553
aitegin-muratbekov <ajteginmuratbekov@gmail>
2022-12-15 16:38:16 +0600

    hw7

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ import os
 import sys
 import dotenv
 
+
 def main():
     dotenv.read_dotenv()
     """Run administrative tasks."""
@@ -17,7 +18,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
